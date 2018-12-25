2cf86bb235af854a014bc98f6f90291081dbb2a9
mingyan.yu <mingyan.yu@wormpex.com>
2018-12-25 19:00:03 +0800

    feat:同步循环

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var traversalStr = require('./modules/traversalStr'),
-    querySite = require('./modules/querySite'),
-    fs = require('fs');
+var traversalStr = require('./modules/traversalStr');
 
 fs.writeFileSync('result.txt', '');
 
 var token = 'check-web-hichina-com%3A5njn5t2ztt89h11887p671ugokhargjn';
 
-traversalStr('miniwqa', 'miniwqz', querySite(token));
+traversalStr('chrisaa', 'chrisaz');
 
--- modules/traversalStr.js
+++ modules/traversalStr.js
@@ -2,6 +2,11 @@
  * 遍历指定字符间的所有字符
  */
 
+    fs = require('fs');
+var token = 'check-web-hichina-com%3A5njn5t2ztt89h11887p671ugokhargjn';
+
+var superagent = require('superagent');
+
 // 字符转数字
 function strToNumber(str) {
     var number = 0;
@@ -35,16 +40,33 @@ function numberToStr(number) {
  * @param  {[type]} dealStrFunc [获取到字符串后的处理函数]
  */
 
-module.exports = function(startStr, endStr, dealStrFunc) {
-    
+module.exports = async function(startStr, endStr) {
+
     if (!/^[a-z]+$/.test(startStr) || !/^[a-z]+$/.test(endStr)) {
         console.log('请输入纯英文字符');
         return false;
     }
+    console.log('开始处理');
     var startNumber = strToNumber(startStr),
         endNumber = strToNumber(endStr);
     for(var i = startNumber; i <= endNumber; i++) {
-        dealStrFunc && dealStrFunc(numberToStr(i));
+        const site = numberToStr(i);
+        console.log('正在处理 ', site);
+        var ans = await superagent.get('https://checkapi.aliyun.com/check/checkdomain?domain=' + site + '.com&token='+ token);
+        if (ans.body.module[0].avail === 1) {
+            console.log(site + '.com is available!')
+            fs.appendFile('result.txt', site + '.com is available\n', (err) => {
+                if (err) throw err;
+            });
+        } else {
+            console.log(site + '.com bad');
+        }
     }
-
+    console.log('处理完毕');
 }
+
+
+
+
+
+
